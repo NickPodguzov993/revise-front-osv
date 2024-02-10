589288fb50212c7b54f17c1b71fcd9cf106f8c7c
NickPodguzov993 <verytallman993@gmail.com>
2024-02-10 19:02:30 +0300

    test4

--- src/entities/revise-object/api.ts
+++ src/entities/revise-object/api.ts
@@ -26,18 +26,18 @@ export async function deleteReviseFile(fileId: ReviseFile["id"]) {
 }
 
 export async function login(username: string, password: string) {
-  return fetch(`/api/user/token?username=${username}&password=${password}`, {
+  return fetch(`/api/user/token`, {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
+  //  mode: "cors", // no-cors, *cors, same-origin
+ //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+  //  credentials: "same-origin", // include, *same-origin, omit
     headers: {
       "Content-Type": "application/json",
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-   // body: JSON.stringify({username, password}), // body data type must match "Content-Type" header
+  //  redirect: "follow", // manual, *follow, error
+   // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify({username, password}), // body data type must match "Content-Type" header
   });
 }
 
