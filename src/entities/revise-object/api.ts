3b5d4417810dd9661e2052480698fefc0cfd985b
NickPodguzov993 <verytallman993@gmail.com>
2024-02-10 19:10:10 +0300

    test5

--- src/entities/revise-object/api.ts
+++ src/entities/revise-object/api.ts
@@ -32,7 +32,7 @@ export async function login(username: string, password: string) {
  //   cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
   //  credentials: "same-origin", // include, *same-origin, omit
     headers: {
-      "Content-Type": "application/json",
+      accept: "application/json",
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
   //  redirect: "follow", // manual, *follow, error
