32426bbee2de147865374652a944db2a8b583e44
Николай Подгузов <npodguzov@it-fintech.com>
2024-02-12 06:28:17 +0000

    change summary/api/ts frpm 100 to 5

--- src/entities/summary/api.ts
+++ src/entities/summary/api.ts
@@ -1,5 +1,5 @@
 import { getMonthDate } from "@/shared/utils";
 
 export function summaryUrl(date: Date, page: number) {
-  return `/api/${getMonthDate(date)}/data?page=${page}&size=100`;
+  return `/api/${getMonthDate(date)}/data?page=${page}&size=5`;
 }
